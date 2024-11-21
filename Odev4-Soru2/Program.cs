67fa9c1fdb93b353691701bf0eae8f1de07141d7
Yusufkr03 <yusufkr2903@gmail.com>
2024-11-21 11:40:25 +0300

    Add files via upload

---
+++ Odev4-Soru2/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics.Metrics;
+using System.Reflection.Metadata;
+using System;
+
+namespace odev
+{
+    class metod
+    {
+        public static int sırala(int[] sayilar)
+        {
+            int max = sayilar[0];
+
+
+            for (int i = 1; i < sayilar.Length; i++)
+            {
+                if (sayilar[i] > max)
+                {
+                    max = sayilar[i];
+                }
+
+            }
+
+            return max;
+            
+
+
+
+        }
+
+        static void Main()
+        {
+            
+            int[] sayilar = new int[5];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                Console.WriteLine("{0}. Sayı", i+1);
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+            int max = metod.sırala(sayilar);
+            Console.WriteLine("En büyük sayı:"+max);
+
+        }
+
+    }
+}
